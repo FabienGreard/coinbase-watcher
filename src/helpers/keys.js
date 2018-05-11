514b7bfd779932dadf4fe1d06ed02dcb57a38f35
Fabien Gréard <grd.fabien@gmail.com>
2018-05-11 14:22:13 +0200

    Create keys.js

---
+++ src/helpers/keys.js
@@ -0,0 +1,5 @@
+export const keys = {
+  apiKey: '709bb2c119f0258a26c11a7539486081',
+  apiSecret:
+    'OY696jH/q3XaAOgNmuiDWIyeAcXMFHYU6s9ZkeciJ3x+EA4YIuMxlHRTZo7ECto9xlRcbrK+ChPi2fiRoSV5Fw=='
+};
